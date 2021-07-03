ec6578a979bcf26099957869039173f0b1217667
Laurentiu Stamate <laurentiu.stamate@outlook.com>
2021-07-03 16:14:11 +0300

    initial project commit

---
+++ 03_TicTacToe_Demo/TicTacToe.Maui/MainPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.Maui.Controls;
+
+namespace TicTacToe.Maui
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ 03_TicTacToe_Demo/TicTacToe.Maui/Startup.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Components.WebView.Maui;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Maui;
+using Microsoft.Maui.Controls.Hosting;
+using Microsoft.Maui.Hosting;
+
+namespace TicTacToe.Maui
+{
+    public class Startup : IStartup
+    {
+        public void Configure(IAppHostBuilder appBuilder)
+        {
+            appBuilder
+                .RegisterBlazorMauiWebView(typeof(Startup).Assembly)
+                .UseMicrosoftExtensionsServiceProviderFactory()
+                .UseMauiApp<App>()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                })
+                .ConfigureServices(services =>
+                {
+                    services.AddBlazorWebView();
+                });
+        }
+    }
+}
