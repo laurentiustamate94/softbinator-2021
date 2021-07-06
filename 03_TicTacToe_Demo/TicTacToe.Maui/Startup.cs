d3e3d6f8e1ace1992c6e889eca88f418701f5c3a
Laurentiu Stamate <laurentiu.stamate@outlook.com>
2021-07-06 23:19:46 +0300

    update other projects

--- 03_TicTacToe_Demo/TicTacToe.Maui/Startup.cs
+++ 03_TicTacToe_Demo/TicTacToe.Maui/Startup.cs
@@ -1,8 +1,12 @@
-﻿using Microsoft.AspNetCore.Components.WebView.Maui;
+﻿using System.IO;
+using Microsoft.AspNetCore.Components.WebView.Maui;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Maui;
 using Microsoft.Maui.Controls.Hosting;
 using Microsoft.Maui.Hosting;
+using Microsoft.Maui.LifecycleEvents;
+using TicTacToe.Services;
 
 namespace TicTacToe.Maui
 {
@@ -21,6 +25,32 @@ namespace TicTacToe.Maui
                 .ConfigureServices(services =>
                 {
                     services.AddBlazorWebView();
+                    services.AddTicTacToe();
+#if WINDOWS
+                    services.AddSingleton<INotificationService, WinUI.Services.NotificationService>();
+#elif ANDROID
+                    services.AddSingleton<INotificationService, Android.Services.NotificationService>();
+#elif IOS
+                    services.AddSingleton<INotificationService, iOS.Services.NotificationService>();
+#endif
+                })
+                .ConfigureAppConfiguration(builder =>
+                {
+#if WINDOWS
+                    builder.AddJsonFile(Path.Combine("Assets", "Resources", "Raw", "appsettings.winui.json"));
+#endif
+                })
+                .ConfigureLifecycleEvents(lifecycle =>
+                {
+#if ANDROID
+                    lifecycle.AddAndroid(d =>
+                    {
+                        d.OnBackPressed(activity =>
+                        {
+                            System.Diagnostics.Debug.WriteLine("Back button pressed!");
+                        });
+                    });
+#endif
                 });
         }
     }
--- 03_TicTacToe_Demo/TicTacToe.Web/Program.cs
+++ 03_TicTacToe_Demo/TicTacToe.Web/Program.cs
@@ -7,6 +7,8 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using TicTacToe.Services;
+using TicTacToe.Web.Services;
 
 namespace TicTacToe.Web
 {
@@ -17,7 +19,9 @@ namespace TicTacToe.Web
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
 
+            builder.Services.AddTicTacToe();
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddSingleton<INotificationService, NotificationService>();
 
             await builder.Build().RunAsync();
         }
