c42b105b3f1c463f03f4c914ff46a36b0198b5bf
Laurentiu Stamate <laurentiu.stamate@outlook.com>
2021-07-07 12:53:50 +0300

    add maui showcase

---
+++ 01_Maui_Demo/MauiApp1/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Maui.Controls;
+
+namespace MauiApp1
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        int count = 0;
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            count++;
+            // count += 4;
+            CounterLabel.Text = $"Current count: {count}";
+
+            //DotNetBotImage.RotateTo(10 * count, 1000);
+        }
+    }
+}
